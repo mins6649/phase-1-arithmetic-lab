2b2986b1577458c4c674752e1e2dbbad80a84173
minyoungshin <mins6649@gmail.com>
2023-01-31 11:21:04 -0500

    math

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 24;
+let num4 = 5;
+let set;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()*10);
+
+const mod = num3 % num4;
+
+const max = Math.max(4, 6, 8, 14, 20, 7);
